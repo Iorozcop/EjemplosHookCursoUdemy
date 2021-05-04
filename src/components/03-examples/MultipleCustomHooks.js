64db93e6da612d6907820c8d339f95aa2bb4736b
Iorozcop <iorozcopuerto@gmail.com>
2021-05-04 13:06:59 +0200

    useFetch funcionando en MultipleCustomHooks

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 
 const MultipleCustomHooks =()=>{
 
+  //se han usado esas comillas para luego poder cambiar el id
+  const state =  useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+
+  const {data,loading,error} = state;
+  // !!data esto significa si hay data y el && data[0] que lo meta en la posicion 0 del array
+  const {quote,author} = !!data && data[0];
+
   return(
     <>
+    <h4>{author}: </h4>
+    <p>{quote}</p>
     </>
   )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 
-const useFetch = () => {
+const useFetch = ( url ) => {
 
+  const [state,setState] = useState({data:null, loading:true, error:null});
+
+  useEffect(()=>{
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setState({
+          loading: false,
+          error:null,
+          data
+        })
+      })
+  },[url])
+
+  return state;
 }
 
 export default useFetch;
