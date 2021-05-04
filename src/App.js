6f0f07c990ffdd49555ecfd348bff9a31464c66d
Iorozcop <iorozcopuerto@gmail.com>
2021-05-04 10:30:24 +0200

    componente MultipleCustomHooks y customHook useFetch creados

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CounterApp from './components/01-useState/CounterApp'
 import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'
 import SimpleForm from './components/02-useEffect/SimpleForm'
 import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook'
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks'
+
 import "./style.css";
 
 export default function App() {
@@ -31,6 +33,13 @@ export default function App() {
         <p>Componente simpleForm con customHook:</p>
         <div>
           <FormWithCustomHook/>
+        </div>
+      </div>
+      <div>
+        <h1>Ejemplo multiples customHook</h1>
+        <hr />
+        <div>
+          <MultipleCustomHooks/>
         </div>
       </div>
     </>
---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MultipleCustomHooks =()=>{
+
+  return(
+    <>
+    </>
+  )
+}
+
+export default MultipleCustomHooks;
